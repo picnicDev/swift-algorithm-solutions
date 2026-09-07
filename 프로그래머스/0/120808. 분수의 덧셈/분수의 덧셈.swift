7d9aa32ae5d04fb6f37b38ce0772d60dd7e71a1c
Sangmin Kim <59194210+picnicDev@users.noreply.github.com>
2026-09-07 19:29:22 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.swift
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.swift
@@ -1,18 +1,15 @@
 import Foundation
 
 func solution(_ numer1:Int, _ denom1:Int, _ numer2:Int, _ denom2:Int) -> [Int] {
-    var numer =  numer1 * denom2 + numer2 * denom1
-    var denom = denom1 * denom2
-    var i = 2
-    
-    while i <= numer {
-        if numer % i == 0 && denom % i == 0 {
-            numer /= i
-            denom /= i
-        } else {
-            i += 1
-        }
+    func gcd(_ a: Int, _ b: Int) -> Int  {
+        if b == 0 { return a }
+        
+        return gcd(b, a % b)
     }
     
-    return [numer, denom]
+    let numer3 = numer1 * denom2 + numer2 * denom1
+    let denom3 = denom1 * denom2
+    let gcd = gcd(max(numer3, denom3), min(numer3, denom3))
+    
+    return [numer3 / gcd, denom3 / gcd]
 }
