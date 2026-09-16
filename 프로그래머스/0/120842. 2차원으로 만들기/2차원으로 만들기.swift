0dc6fbe8a6bccacd1905122a1a8ff4ca2b0aa95b
Sangmin Kim <59194210+picnicDev@users.noreply.github.com>
2026-09-16 13:52:26 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.07 ms, Memory: 20.2 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.swift
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.swift
@@ -1,16 +1,11 @@
 import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> [[Int]] {
-    var result: [[Int]] = []
+    var answer: [[Int]] = []
     
     for i in stride(from: 0, to: num_list.count, by: n) {
-        var arr: [Int] = []
-        for j in 0..<n {
-            arr.append(num_list[i+j])
-        }
-        
-        result.append(arr)
+        answer.append(Array(num_list[i..<i+n]))
     }
     
-    return result
+    return answer
 }
