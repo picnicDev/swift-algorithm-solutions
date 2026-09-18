4d86c01da04953063b0258ab6ad7857febe41844
Sangmin Kim <59194210+picnicDev@users.noreply.github.com>
2026-09-18 09:28:33 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 20.6 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.swift
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.swift
@@ -1,18 +1,16 @@
 import Foundation
 
 func solution(_ n:Int) -> [Int] {
-    var n = n 
-    var result: Set<Int> = []
-    var div = 2
-    
-    while n != 1 {
-        if n % div == 0 {
-            n /= div
-            result.insert(div)
-        } else {
-            div += 1
+    var result = [Int]()
+    var n = n
+    for i in 2...n {
+        if n % i == 0 {
+            result.append(i)
+            while n % i == 0 {
+                n /= i
+            }
         }
     }
     
-    return result.sorted()
+    return result
 }
