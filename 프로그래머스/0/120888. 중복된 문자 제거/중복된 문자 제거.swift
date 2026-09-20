17445caa1921b00af900e80da34b3abff67657c2
Sangmin Kim <59194210+picnicDev@users.noreply.github.com>
2026-09-21 08:03:15 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.11 ms, Memory: 20.1 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.swift
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.swift
@@ -1,16 +1,18 @@
 import Foundation
 
 func solution(_ my_string:String) -> String {
-    var arr: [Character] = Array(my_string)
-    var result: [Character] = []
-    var usedChar: Set<Character> = []
+    // set에 포함되어 있으면 ""로 바꾸기? -> 바꾸는데 시간이 걸릴 듯
+    // 하나씩 추가하면서 새로 만드는게 나을수도
+    // 공백도 여기에 포함되네?
+    var used = Set<Character>()
+    var result = ""
     
-    for c in arr {
-        if !usedChar.contains(c) {
-            result.append(c)
-            usedChar.insert(c)
-        }
+    for c in my_string {
+        if used.contains(c) { continue }
+        
+        used.insert(c)
+        result.append(c)
     }
     
-    return String(result)
+    return result
 }
