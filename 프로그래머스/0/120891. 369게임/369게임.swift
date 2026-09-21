417e02d9553f495392ae1330fe32ecbcc99964aa
Sangmin Kim <59194210+picnicDev@users.noreply.github.com>
2026-09-22 08:40:50 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 20.3 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.swift
+++ 프로그래머스/0/120891. 369게임/369게임.swift
@@ -1,16 +1,5 @@
 import Foundation
 
 func solution(_ order:Int) -> Int {
-    var result = 0
-    
-    for c in String(order) {
-        switch Int(String(c))! {
-            case 3: fallthrough
-            case 6: fallthrough
-            case 9: result += 1
-            default: continue
-        }
-    }
-    
-    return result
+    return String(order).count(where: { $0 == "3" || $0 == "6" || $0 == "9" })
 }
