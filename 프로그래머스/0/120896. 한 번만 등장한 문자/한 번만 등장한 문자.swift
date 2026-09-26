dfa3bd25f43671f72e25e380a5fc915439ff11f7
Sangmin Kim <59194210+picnicDev@users.noreply.github.com>
2026-09-26 14:50:13 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.10 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.swift
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.swift
@@ -1,19 +1,19 @@
 import Foundation
 
 func solution(_ s:String) -> String {
-     var arr = Array(repeating: 0, count: 26)
-    var result: [Character] = []
+    var alphabetCountArr = Array(repeating: 0, count: 26)
     
     for c in s {
-        let i = Int(c.asciiValue! - 97)
-        arr[i] += 1
+        let index = Int(c.asciiValue! - 97)
+        alphabetCountArr[index] += 1
     }
     
-    for i in 0..<arr.count {
-        if arr[i] == 1 {
-            result.append(Character(UnicodeScalar(i+97)!))
-        }
-    }
+    let result = alphabetCountArr.enumerated().compactMap { 
+        if $0.1 == 1 { return $0.0 }
+        return nil
+    }.map {
+        String(UnicodeScalar(97+$0)!)
+    }.joined()
     
-    return String(result)
+    return result
 }
